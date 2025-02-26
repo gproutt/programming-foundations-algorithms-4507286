c99027b896131ad2c9cf8457330dfa493328fe17
gproutt <gproutt@gmail.com>
2025-02-26 18:36:32 +0000

    Added GCD logic

--- Start/Introduction/gcd_start.py
+++ Start/Introduction/gcd_start.py
@@ -2,9 +2,22 @@
 # Find the greatest common denominator of two numbers
 # using Euclid's algorithm
 
+# Logic: 
+# 1. For two integers a and b, where a > b, divide a by b
+# 2. If the remainder, r, is 0 then stop: GCD is b. 
+# 3. Otherwise, set a to b, b to r, and repeat at step 1 until r is 0.
 
 def gcd(a, b):
-    pass
+    while(b != 0): # gcd of 20 and 8 is 4
+        t = a # store the value of a in a temporary variable
+        print("t=",t)
+        a = b # set a equal to b (in case there is another loop)
+        print("a=",a)
+        b = t % b # recalculate b to be the remainder of a divided by b
+        print("b=",b)
+        # if b is now zero, the loop will stop
+        # otherwise, a becomes b, and b becomes the remainder for the next loop
+    return a
 
 
 # try out the function with a few examples
